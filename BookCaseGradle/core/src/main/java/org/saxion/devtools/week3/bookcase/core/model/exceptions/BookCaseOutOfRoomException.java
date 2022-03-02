2dc48f213cb3ba49fafaf5545b0fc2b52dd8a86c
kristiyan2021 <449302@student.saxion.nl>
2022-03-02 12:22:56 +0100

    wk3(bookcase): Multi-project build

---
+++ BookCaseGradle/core/src/main/java/org/saxion/devtools/week3/bookcase/core/model/exceptions/BookCaseOutOfRoomException.java
@@ -0,0 +1,6 @@
+package org.saxion.devtools.week3.bookcase.core.model.exceptions;
+
+public class BookCaseOutOfRoomException extends Exception {
+
+
+}
---
+++ BookCaseGradle/core/src/main/java/org/saxion/devtools/week3/bookcase/core/model/exceptions/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package org.saxion.devtools.week3.bookcase.core.model.exceptions;
+
+public class BookNotFoundException extends Exception {
+
+    public BookNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
