49de1b7df3965ecef0c9e58648b00b974a2307ef
Quan <490848@student.saxion.nl>
2022-03-30 14:18:26 +0200

    ZOO PROJECT!

---
+++ Zoo/app/src/main/java/zooApplication/gui/UseCaseHandler.java
@@ -0,0 +1,18 @@
+package zooApplication.gui;
+
+/**
+ * The main application should implement this interface which suplies the
+ * handleUseCase method which is invoked by the class UseCaseSet, whenever a
+ * usecase Button is clicked.
+ */
+public interface UseCaseHandler {
+
+	/**
+	 * Called with the name of the selected usecase.
+	 * 
+	 * @param useCaseName
+	 *            The name of this usecase.
+	 */
+	void handleUseCase(String useCaseName);
+
+}
